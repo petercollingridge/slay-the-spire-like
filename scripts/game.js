88adf4ac0a0538ab215db7806a6c050a4db46895
Peter Collingridge <peter.collingridge@nanoporetech.com>
2023-12-17 22:30:43 +0000

    Reorganise dragging code

--- scripts/game.js
+++ scripts/game.js
@@ -1,48 +1,46 @@
-class Example extends Phaser.Scene {
-  preload () {
+const WIDTH = 1000;
+const HEIGHT = 500;
+
+class Game extends Phaser.Scene {
+  preload() {
     this.load.image('background', 'assets/background.svg');
     this.load.image('enemy-1', 'assets/sasquatch.svg');
     this.load.image('card', 'assets/card.svg');
   }
 
-  create () {
-    this.add.image(500, 300, 'background');
+  create() {
+    this.add.image(WIDTH / 2, HEIGHT / 2, 'background');
     this.add.image(850, 340, 'enemy-1');
 
-    this.hand = new Hand(this, 500, 600);
+    this.hand = new Hand(this, WIDTH / 2, HEIGHT);
     this.hand.addCards(4);
 
-    const dragStart = (pointer, target) => {
-      target.setTint(0x0066bb);
-      this.hand.bringToFront(target);
-    };
-    
-    const dragEnd = (pointer, target) => {
-      target.clearTint();
-    };
-    
-    const drag = (pointer, target, dragX, dragY) => {
-      target.x = dragX;
-      target.y = dragY;
-    };
-
-    this.input.on('dragstart', dragStart);
+    this.input.on('dragstart', dragStart.bind(this));
     this.input.on('drag', drag);
-    this.input.on('dragend', dragEnd);
+    this.input.on('dragend', dragEnd.bind(this));
   }
+}
+
+function dragStart(pointer, target) {
+  target.setTint(0x0066bb);
+  this.hand.bringToFront(target);
+}
+
+function drag(pointer, target, dragX, dragY) {
+  target.x = dragX;
+  target.y = dragY;
+}
+
+function dragEnd(pointer, target) {
+  target.clearTint();
+  // if (target.x <)
 }
 
 const config = {
   type: Phaser.AUTO,
-  width: 1000,
-  height: 600,
-  scene: Example,
-  physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 400 }
-      }
-  }
+  width: WIDTH,
+  height: HEIGHT,
+  scene: Game,
 };
 
 const game = new Phaser.Game(config);
