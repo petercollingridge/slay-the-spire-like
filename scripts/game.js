8e08c1154970de1ace5ed63250dd95b4eb02fec6
Peter Collingridge <peter.collingridge@nanoporetech.com>
2023-12-20 23:05:29 +0000

    Player to draw to 5 at the start of turn

--- scripts/game.js
+++ scripts/game.js
@@ -8,6 +8,8 @@ class Game extends Phaser.Scene {
     this.load.image('card', 'assets/cards/card-base.svg');
     this.load.image('attack', 'assets/cards/attack.svg');
     this.load.image('attack-2', 'assets/cards/mighty-sword.svg');
+    this.load.image('heart', 'assets/cards/heart.svg');
+    this.load.image('shield', 'assets/cards/shield.svg');
     this.load.image('draw-card', 'assets/cards/draw.svg');
   }
 
@@ -50,9 +52,16 @@ class Game extends Phaser.Scene {
     }
   }
 
-  drawCards(n) {
-    for (let i = 0; i < n; i++) {
+  drawCards(nCards) {
+    for (let i = 0; i < nCards; i++) {
       this.drawCard();
+    }
+  }
+
+  drawCardsTo(maxCards) {
+    const nCards = maxCards - this.hand.cards.length;
+    if (nCards > 0) {
+      this.drawCards(nCards);
     }
   }
 
@@ -62,6 +71,11 @@ class Game extends Phaser.Scene {
 
   enemyTurn() {
     this.player.dealDamage(this.enemy.attack);
+    this.playerTurn();
+  }
+
+  playerTurn() {
+    this.drawCardsTo(5);
   }
 }
 
